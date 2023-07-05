8c0366b14be1c5b7b95682b243f7931bc1b9204f
rasty <hvozdetskiyyrii@gmail.com>
2023-07-05 15:46:09 +0300

    made getcovers-useful-resources page

--- app/blocks/develop/app/assets/refs.js
+++ app/blocks/develop/app/assets/refs.js
@@ -1,11 +1,11 @@
 "use strict";
 /* ============================ refs START ================================================================= */
 const refs = Object.freeze({
-	dropDownList: $('.dropdown__list'),
-	dropDownBtn: $('.dropdown__button'),
-	dropDownItems: $('.dropdown__item'),
-	dropDownInput: $('.dropdown__input-hidden'),
-	footer:$('footer')
+	// dropDownList: $('.dropdown__list'),
+	// dropDownBtn: $('.dropdown__button'),
+	// dropDownItems: $('.dropdown__item'),
+	// dropDownInput: $('.dropdown__input-hidden'),
+	// footer:$('footer')
 })
 
 /* ============================ refs END ================================================================= */
--- app/blocks/develop/app/assets/select_dropdown.js
+++ app/blocks/develop/app/assets/select_dropdown.js
@@ -1,57 +1,59 @@
-/* ============================ select_dropdown.js START ================================================================= */
-$(document).on('click', '.dropdown__button', function () {
-	refs.dropDownList.toggleClass('dropdown__list_visible')
-	refs.dropDownBtn.toggleClass('active')
-})
-
-$(document).on('click', '.dropdown__list', function (e) {
-	const target = $(e.target)
-	const textElem = target.text()
-	const dataValueElem = target.data('value')
-
-	refs.dropDownBtn.text(textElem)
-	refs.dropDownInput.val(dataValueElem)
-	refs.dropDownList.toggleClass('dropdown__list_visible')
-
-	searchElDropdown();
-})
-
-// приховує з списку мову яку вибрали
-const searchElDropdown = () => {
-	const valueBtn = refs.dropDownBtn.text().toUpperCase()
-
-	refs.dropDownItems.each(function () {
-		const ths = this
-		const valueEl = ths.textContent.toUpperCase()
-		if (valueEl === valueBtn) ths.classList.add('hidden')
-		if (valueEl !== valueBtn) ths.classList.remove('hidden')
-	})
-}
-
-// Нажатие на Escape. Закрыть дропдаун
-$(document).on('keydown', function (e) {
-	if (e.key === 'Escape') {
-		refs.dropDownList.removeClass('dropdown__list_visible')
-		refs.dropDownBtn.removeClass('active')
-	}
-});
-
-// Клик снаружи дропдауна. Закрыть дропдаун
-$(document).on('click', function (e) {
-	//TODO як переписати це на JQ ?
-	// const target = $(e.target)
-	// console.log('e.target',e.target)
-	// console.log('refs.dropDownBtn',refs.dropDownBtn)
-	// console.log('target',target)
-	const dropDownBtn = document.querySelector('.dropdown__button');
-
-	if (e.target !== dropDownBtn) {
-		refs.dropDownList.removeClass('dropdown__list_visible')
-		refs.dropDownBtn.removeClass('active')
-	}
-});
-/* ============================ select_dropdown.js END ================================================================= */
-
-
-
-
+// /*
+// /!* ============================ select_dropdown.js START ================================================================= *!/
+// $(document).on('click', '.dropdown__button', function () {
+// 	refs.dropDownList.toggleClass('dropdown__list_visible')
+// 	refs.dropDownBtn.toggleClass('active')
+// })
+//
+// $(document).on('click', '.dropdown__list', function (e) {
+// 	const target = $(e.target)
+// 	const textElem = target.text()
+// 	const dataValueElem = target.data('value')
+//
+// 	refs.dropDownBtn.text(textElem)
+// 	refs.dropDownInput.val(dataValueElem)
+// 	refs.dropDownList.toggleClass('dropdown__list_visible')
+//
+// 	searchElDropdown();
+// })
+//
+// // приховує з списку мову яку вибрали
+// const searchElDropdown = () => {
+// 	const valueBtn = refs.dropDownBtn.text().toUpperCase()
+//
+// 	refs.dropDownItems.each(function () {
+// 		const ths = this
+// 		const valueEl = ths.textContent.toUpperCase()
+// 		if (valueEl === valueBtn) ths.classList.add('hidden')
+// 		if (valueEl !== valueBtn) ths.classList.remove('hidden')
+// 	})
+// }
+//
+// // Нажатие на Escape. Закрыть дропдаун
+// $(document).on('keydown', function (e) {
+// 	if (e.key === 'Escape') {
+// 		refs.dropDownList.removeClass('dropdown__list_visible')
+// 		refs.dropDownBtn.removeClass('active')
+// 	}
+// });
+//
+// // Клик снаружи дропдауна. Закрыть дропдаун
+// $(document).on('click', function (e) {
+// 	//TODO як переписати це на JQ ?
+// 	// const target = $(e.target)
+// 	// console.log('e.target',e.target)
+// 	// console.log('refs.dropDownBtn',refs.dropDownBtn)
+// 	// console.log('target',target)
+// 	const dropDownBtn = document.querySelector('.dropdown__button');
+//
+// 	if (e.target !== dropDownBtn) {
+// 		refs.dropDownList.removeClass('dropdown__list_visible')
+// 		refs.dropDownBtn.removeClass('active')
+// 	}
+// });
+// /!* ============================ select_dropdown.js END ================================================================= *!/
+//
+//
+//
+//
+// */
